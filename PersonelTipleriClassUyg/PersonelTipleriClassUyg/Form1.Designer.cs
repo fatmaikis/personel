12e50fd579c32be3400a35d7c0687f44b8ab1a78
Fatma Ikış <77547782+fatmaikis@users.noreply.github.com>
2022-12-21 13:59:55 +0300

    Add files via upload

---
+++ PersonelTipleriClassUyg/PersonelTipleriClassUyg/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace PersonelTipleriClassUyg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.aditxt = new System.Windows.Forms.TextBox();
+            this.ssktxt = new System.Windows.Forms.TextBox();
+            this.maastxt = new System.Windows.Forms.TextBox();
+            this.zamtxt = new System.Windows.Forms.TextBox();
+            this.departmankartxt = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.satistxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.eklebtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(745, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personel Tipleri";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(507, 26);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(105, 33);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "Satışçı";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged_1);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(229, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(114, 33);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Çalışan";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged_1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(380, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 33);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Müdür";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(294, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(264, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ssk No :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(227, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zam Miktarı :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(279, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Maaş :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(196, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Departman Kar :";
+            // 
+            // aditxt
+            // 
+            this.aditxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.aditxt.Location = new System.Drawing.Point(383, 75);
+            this.aditxt.Name = "aditxt";
+            this.aditxt.Size = new System.Drawing.Size(100, 30);
+            this.aditxt.TabIndex = 6;
+            // 
+            // ssktxt
+            // 
+            this.ssktxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ssktxt.Location = new System.Drawing.Point(383, 111);
+            this.ssktxt.Name = "ssktxt";
+            this.ssktxt.Size = new System.Drawing.Size(100, 30);
+            this.ssktxt.TabIndex = 7;
+            // 
+            // maastxt
+            // 
+            this.maastxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maastxt.Location = new System.Drawing.Point(383, 147);
+            this.maastxt.Name = "maastxt";
+            this.maastxt.Size = new System.Drawing.Size(100, 30);
+            this.maastxt.TabIndex = 8;
+            // 
+            // zamtxt
+            // 
+            this.zamtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.zamtxt.Location = new System.Drawing.Point(383, 184);
+            this.zamtxt.Name = "zamtxt";
+            this.zamtxt.Size = new System.Drawing.Size(100, 30);
+            this.zamtxt.TabIndex = 9;
+            // 
+            // departmankartxt
+            // 
+            this.departmankartxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.departmankartxt.Location = new System.Drawing.Point(383, 221);
+            this.departmankartxt.Name = "departmankartxt";
+            this.departmankartxt.Size = new System.Drawing.Size(100, 30);
+            this.departmankartxt.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 379);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(735, 148);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(12, 351);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Personel Listesi";
+            // 
+            // satistxt
+            // 
+            this.satistxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.satistxt.Location = new System.Drawing.Point(383, 259);
+            this.satistxt.Name = "satistxt";
+            this.satistxt.Size = new System.Drawing.Size(100, 30);
+            this.satistxt.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(220, 262);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Satış Sayısı : ";
+            // 
+            // eklebtn
+            // 
+            this.eklebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.eklebtn.Location = new System.Drawing.Point(345, 317);
+            this.eklebtn.Name = "eklebtn";
+            this.eklebtn.Size = new System.Drawing.Size(165, 44);
+            this.eklebtn.TabIndex = 15;
+            this.eklebtn.Text = "Ekle";
+            this.eklebtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(760, 538);
+            this.Controls.Add(this.eklebtn);
+            this.Controls.Add(this.satistxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.departmankartxt);
+            this.Controls.Add(this.zamtxt);
+            this.Controls.Add(this.maastxt);
+            this.Controls.Add(this.ssktxt);
+            this.Controls.Add(this.aditxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox aditxt;
+        private System.Windows.Forms.TextBox ssktxt;
+        private System.Windows.Forms.TextBox maastxt;
+        private System.Windows.Forms.TextBox zamtxt;
+        private System.Windows.Forms.TextBox departmankartxt;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox satistxt;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button eklebtn;
+    }
+}
+
---
+++ PersonelTipleriClassUyg/PersonelTipleriClassUyg/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonelTipleriClassUyg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label1.Visible = true;
+                aditxt.Visible = true;
+                label2.Visible = true;
+                ssktxt.Visible = true;
+                label3.Visible = true;
+                zamtxt.Visible = true;
+                label4.Visible = true;
+                maastxt.Visible = true;
+                departmankartxt.Visible = false;
+                label5.Visible = false;
+                satistxt.Visible = false;
+                label7.Visible = false;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                label1.Visible = true;
+                aditxt.Visible = true;
+                label2.Visible = true;
+                ssktxt.Visible = true;
+                label3.Visible = true;
+                zamtxt.Visible = true;
+                label4.Visible = true;
+                maastxt.Visible = true;
+                label5.Visible = true;
+                departmankartxt.Visible = true;
+                satistxt.Visible = false;
+                label7.Visible = false;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                label1.Visible = true;
+                aditxt.Visible = true;
+                label2.Visible = true;
+                ssktxt.Visible = true;
+                label3.Visible = true;
+                zamtxt.Visible = true;
+                label4.Visible = true;
+                maastxt.Visible = true;
+                label5.Visible = true;
+                departmankartxt.Visible = false;
+                label5.Visible = false;
+                satistxt.Visible = true;
+                label7.Visible = true;
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+
+
+        }
+
+        private void eklebtn_Click(object sender, EventArgs e)
+        {
+
+
+
+            Calisan calisan = new Calisan();
+            calisan.AD = aditxt.Text;
+            calisan.SSKNO = Convert.ToInt32(ssktxt.Text);
+            calisan.MAAS = Convert.ToDouble(maastxt.Text);
+            calisan.ZamYap(Convert.ToDouble(zamtxt.Text));
+            
+            //listBox1.Items.Add(calisan.ZamYap());
+
+
+
+
+            Mudur mudur = new Mudur();
+            mudur.AD = aditxt.Text;
+            mudur.SSKNO = Convert.ToInt32(ssktxt.Text);
+            mudur.MAAS = Convert.ToDouble(maastxt.Text);
+            mudur.DEPARTMANKAR = Convert.ToDouble(departmankartxt.Text);
+            mudur.ZamYap(Convert.ToDouble(zamtxt.Text));
+
+
+
+
+            Satisci satisci = new Satisci();
+            satisci.AD = aditxt.Text;
+            satisci.SSKNO = Convert.ToInt32(ssktxt.Text);
+            satisci.MAAS = Convert.ToDouble(maastxt.Text);
+            satisci.SATISSAYISI = Convert.ToInt32(satistxt.Text);
+            satisci.ZamYap(Convert.ToDouble(zamtxt.Text));
+
+
+
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calisan calisan = new Calisan();
+            listBox1.Items.Add(calisan.AD);
+            listBox1.Items.Add(calisan.SSKNO);
+            listBox1.Items.Add(calisan.MAAS);
+
+
+            Mudur mudur = new Mudur();
+          
+        }
+
+        private void radioButton3_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Calisan : IDisposable
+    {
+        private int sskNo;
+
+        public int SSKNO
+        {
+            get { return sskNo; }
+            set { sskNo = value; }
+        }
+
+        private string Adi;
+
+        public string AD
+        {
+            get { return Adi; }
+            set { Adi = value; }
+        }
+
+        protected double maas;
+
+        public double MAAS
+        {
+            get { return maas; }
+            set { maas = value; }
+        }
+
+        public virtual void ZamYap(double zam)
+        {
+            MAAS += zam;
+        }
+
+        public virtual string BilgiVer()
+        {
+            return this.SSKNO + "/" + this.AD + "/" + this.MAAS;
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    } 
+
+    class Mudur : Calisan
+    {
+        private double departmanKar;
+
+        public double DEPARTMANKAR
+        {
+            get { return departmanKar; }
+            set { departmanKar = value; }
+        }
+
+        public override void ZamYap(double zam)
+        {
+            double ekstraZam = 0;
+
+            if (DEPARTMANKAR > 0 && DEPARTMANKAR < 100000)
+            {
+                ekstraZam = 10000;
+            }
+            else if (DEPARTMANKAR > 100000)
+                ekstraZam = 15000;
+
+            else if (DEPARTMANKAR < 0)
+                ekstraZam = -3000;
+            MAAS += zam + ekstraZam;
+
+
+        }
+    }
+
+    class Satisci : Calisan
+    {
+        private int satisSayisi;
+
+        public int SATISSAYISI
+        {
+            get { return satisSayisi; }
+            set { satisSayisi = value; }
+
+
+        }
+
+        public override void ZamYap(double zam)
+        {
+            double ekstraZam = 0;
+
+            if (SATISSAYISI > 0 && SATISSAYISI < 100000)
+            {
+                ekstraZam = 10000;
+            }
+            else if (SATISSAYISI > 100000)
+                ekstraZam = 15000;
+
+            else if (SATISSAYISI < 0)
+                ekstraZam = -3000;
+            MAAS += zam;
+
+        }
+
+        public override string BilgiVer()
+        {
+            return base.BilgiVer() + " / " + this.SATISSAYISI;
+
+        }
+
+    }
+
+
+
+}
